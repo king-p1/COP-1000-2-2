502fcc0dc97de9635dde8e590114561d12b4b977
Oladele  Paul <98545430+king-p1@users.noreply.github.com>
2025-09-19 20:15:15 -0400

    Delete Assignment 2-2.py.py

--- Assignment 2-2.py.py
+++
@@ -1,7 +0,0 @@
-myCurrentAge = 18         
-currentYear = 2025       
-
-ageIn2050 = myCurrentAge + (2050 - currentYear)
-
-print("I am currently", myCurrentAge, "years old in", currentYear)
-print("In the year 2050, I will be", ageIn2050, "years old.")
